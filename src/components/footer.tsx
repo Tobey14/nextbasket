8e7b36d714feae61a9c074a5330abae17f474188
Tobey14 <sureboytobi@gmail.com>
2024-01-20 16:51:39 +0100

    Initial Commit

---
+++ src/components/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <main className="flex">
+      Footer
+    </main>
+  );
+}
---
+++ src/components/header.tsx
@@ -0,0 +1,19 @@
+export default function Header() {
+    return (
+      <main className="">
+        <section className="bg-[#23856D] h-[58px] flex items-center justify-between">
+            <div className="flex items-center px-5">
+                <p className="mr-2">
+                    (225) 555-0118
+                </p>
+
+                <p className="">
+                    michelle.rivera@example.com
+                </p>
+
+            </div>
+        </section>
+        
+      </main>
+    );
+}
