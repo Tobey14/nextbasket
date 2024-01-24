2fcc2600585f8eced2344cbbec136328b081e2ed
Tobey14 <sureboytobi@gmail.com>
2024-01-24 09:27:21 +0100

    api call fixes - deployment

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,13 +1,16 @@
-import axios,{ AxiosRequestConfig } from 'axios'
+import axios,{ AxiosRequestConfig, AxiosResponse} from 'axios'
 
 const baseURL = process.env.NEXT_PUBLIC_BASE_URL
     , isServer = typeof window === 'undefined'
 
-const api:AxiosRequestConfig = axios.create({
+const config:AxiosRequestConfig = {
     baseURL,
     headers: {
         'Content-Type': 'application/json'
     }
-})
+}
+
+const api:AxiosResponse = axios.create(config);
+
 
 export default api
